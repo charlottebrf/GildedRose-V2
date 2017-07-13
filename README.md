277b1e35755679e16009da6995a49aec2ca062be
Charlotte Fereday <cbrfereday@gmail.com>
2017-07-13 08:31:29 +0100

    Adds in start of Golden Master tests - need to fix Coverage

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # The GildedRose Kata
-A second attempt at The GildedRose during Makers Academy Tech Test Week. We had limited time to make progress with this kata of about 4 hours.
+A second attempt at The GildedRose during Makers Academy Tech Test Week. We had limited time to make progress with this kata of about 4 hours
 
 
 # Getting started
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,4 +1,5 @@
 require 'gilded_rose'
+require 'spec_helper'
 
 describe GildedRose do
 
@@ -8,6 +9,37 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    it "does not change the sell in value" do
+      items = [Item.new("foo", 1, 2)]
+      GildedRose.new(items).update_quality()
+      p items
+      expect(items[0].sell_in).to eq 0
+    end
+
+    it "does not change the sell in value" do
+      items = [Item.new("foo", 1, 2)]
+      GildedRose.new(items).update_quality()
+      p items
+      expect(items[0].sell_in).to eq 0
+    end
+
+    it "prints the end result" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 2)]
+      rose = GildedRose.new(items)
+      30.times { rose.update_quality() }
+      p 'These are the items!'
+      p rose.items
+      expect(items[0].name).to eq "Aged Brie"
+      expect(items[0].sell_in).to eq -29
+      expect(items[0].quality).to eq 50
+      # [#<Item:0x007fc868026c18 @name="Aged Brie", @sell_in=-29, @quality=50>]
+      # [#<Item:0x007fa27401e398 @name="Sulfuras, Hand of Ragnaros", @sell_in=1, @quality=2>]
+      # [#<Item:0x007ffc1db29d20 @name="Backstage passes to a TAFKAL80ETC concert", @sell_in=-29, @quality=0>]
+    end
+
+
   end
+
 
 end
